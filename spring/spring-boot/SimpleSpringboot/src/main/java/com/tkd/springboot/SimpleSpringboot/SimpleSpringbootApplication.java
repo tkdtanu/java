36942ece5037a4a8ece1e763d297d5e68d84d8ec
tkdtanu <tkdtanu@gmail.com>
2017-12-17 23:15:15 +0530

    added project

---
+++ spring/spring-boot/SimpleSpringboot/src/main/java/com/tkd/springboot/SimpleSpringboot/SimpleSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tkd.springboot.SimpleSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootApplication.class, args);
+	}
+}
